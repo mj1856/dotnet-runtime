fa06eaa7a62e6062d0cd8958ae24db395ce0350b
Matt Johnson-Pint <mattjoh@microsoft.com>
2021-03-05 10:56:53 -0800

    define constants, use one less temp string, invert conditions to decrease indentation

--- src/libraries/Native/Unix/System.Globalization.Native/pal_timeZoneInfo.c
+++ src/libraries/Native/Unix/System.Globalization.Native/pal_timeZoneInfo.c
@@ -10,6 +10,9 @@
 #include "pal_locale_internal.h"
 #include "pal_timeZoneInfo.h"
 
+#define DISPLAY_NAME_LENGTH 256  // arbitrarily large, to be safe
+#define TZID_LENGTH 64           // arbitrarily large, to be safe
+
 // For descriptions of the following patterns, see https://unicode-org.github.io/icu/userguide/format_parse/datetime/#date-field-symbol-table
 static const UChar GENERIC_PATTERN_UCHAR[] = {'v', 'v', 'v', 'v', '\0'};           // u"vvvv"
 static const UChar GENERIC_LOCATION_PATTERN_UCHAR[] = {'V', 'V', 'V', 'V', '\0'};  // u"VVVV"
@@ -210,14 +213,14 @@ void FixupTimeZoneGenericDisplayName(
 
     // Get the standard name for this time zone.  (-1 == timeZoneId is null terminated)
     // Note that we leave the calendar open and close it later so we can also get the base offset.
-    UChar standardName[256];
+    UChar standardName[DISPLAY_NAME_LENGTH];
     UCalendar* calendar = ucal_open(timeZoneId, -1, locale, UCAL_DEFAULT, err);
     if (U_FAILURE(*err)) goto exit;
 
     ucal_setMillis(calendar, timestamp, err);
     if (U_FAILURE(*err)) goto exit;
 
-    ucal_getTimeZoneDisplayName(calendar, UCAL_STANDARD, locale, standardName, 256, err);
+    ucal_getTimeZoneDisplayName(calendar, UCAL_STANDARD, locale, standardName, DISPLAY_NAME_LENGTH, err);
     if (U_FAILURE(*err)) goto exit;
 
     // See if the generic name is the same as the standard name.
@@ -229,10 +232,9 @@ void FixupTimeZoneGenericDisplayName(
         if (U_FAILURE(*err)) goto exit;
 
         // Allocate some additional strings for test values.
-        UChar testTimeZoneId[64];
-        UChar testStandardName[256];
-        UChar testGenericName[256];
-        UChar testGenericLocationName[256];
+        UChar testTimeZoneId[TZID_LENGTH];
+        UChar testDisplayName[DISPLAY_NAME_LENGTH];
+        UChar testDisplayName2[DISPLAY_NAME_LENGTH];
 
         // Enumerate over all the time zones having the same base offset.
         UEnumeration* pEnum = ucal_openTimeZoneIDEnumeration(UCAL_ZONE_TYPE_CANONICAL_LOCATION, NULL, &baseOffset, err);
@@ -250,34 +252,40 @@ void FixupTimeZoneGenericDisplayName(
             u_uastrcpy(testTimeZoneId, testId);
 
             // Get the standard name from the test time zone.
-            GetTimeZoneDisplayName_FromCalendar(locale, testTimeZoneId, timestamp, UCAL_STANDARD, testStandardName, 256, err);
+            GetTimeZoneDisplayName_FromCalendar(locale, testTimeZoneId, timestamp, UCAL_STANDARD, testDisplayName, DISPLAY_NAME_LENGTH, err);
             if (U_FAILURE(*err)) goto exitEnum;
 
-            // See if the test time zone has the same standard name.
-            if (u_strcmp(testStandardName, standardName) == 0)
+            // See if the test time zone has a different standard name.
+            if (u_strcmp(testDisplayName, standardName) != 0)
             {
-                // Get the generic name from the test time zone.
-                GetTimeZoneDisplayName_FromPattern(locale, testTimeZoneId, timestamp, GENERIC_PATTERN_UCHAR, testGenericName, 256, err);
-                if (U_FAILURE(*err)) goto exitEnum;
+                // It has a different standard name. We can't use it.
+                continue;
+            }
 
-                // See if the test time zone has a shorter generic name.
-                if (u_strlen(testGenericName) < originalGenericNameActualLength)
-                {
-                    // Just to be safe, make sure the test zone isn't using a generic name that is specific to a
-                    // particular location.  For example, "Antarctica/Troll" uses "Troll Time" as
-                    // a generic name, but "Greenwich Mean Time" as a standard name.  We don't
-                    // want other zones that use "Greenwich Mean Time" to be labeled as "Troll Time".
+            // Get the generic name from the test time zone.
+            GetTimeZoneDisplayName_FromPattern(locale, testTimeZoneId, timestamp, GENERIC_PATTERN_UCHAR, testDisplayName, DISPLAY_NAME_LENGTH, err);
+            if (U_FAILURE(*err)) goto exitEnum;
 
-                    GetTimeZoneDisplayName_FromPattern(locale, testTimeZoneId, timestamp, GENERIC_LOCATION_PATTERN_UCHAR, testGenericLocationName, 256, err);
-                    if (U_FAILURE(*err)) goto exitEnum;
+            // See if the test time zone has a longer (or same size) generic name.
+            if (u_strlen(testDisplayName) >= originalGenericNameActualLength)
+            {
+                // The test time zone's generic name isn't any shorter than the one we already have.
+                continue;
+            }
 
-                    if (u_strcmp(testGenericName, testGenericLocationName) != 0)
-                    {
-                        // We have found a better generic name.  Use it.
-                        u_strcpy(genericName, testGenericName);
-                        break;
-                    }
-                }
+            // We probably have found a better generic name.  But just to be safe, make sure the test zone isn't
+            // using a generic name that is specific to a particular location.  For example, "Antarctica/Troll"
+            // uses "Troll Time" as a generic name, but "Greenwich Mean Time" as a standard name.  We don't
+            // want other zones that use "Greenwich Mean Time" to be labeled as "Troll Time".
+
+            GetTimeZoneDisplayName_FromPattern(locale, testTimeZoneId, timestamp, GENERIC_LOCATION_PATTERN_UCHAR, testDisplayName2, DISPLAY_NAME_LENGTH, err);
+            if (U_FAILURE(*err)) goto exitEnum;
+
+            if (u_strcmp(testDisplayName, testDisplayName2) != 0)
+            {
+                // We have found a better generic name.  Use it.
+                u_strcpy(genericName, testDisplayName);
+                break;
             }
         }
 
